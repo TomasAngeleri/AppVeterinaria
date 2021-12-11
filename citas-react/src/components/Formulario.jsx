186bd8acf692ab0aeba7dcab13068cee56dab5af
TomasAngeleri <44233271+TomasAngeleri@users.noreply.github.com>
2021-12-11 00:43:06 -0300

    Estilos con tailwindcss a los componentes principales de la app

---
+++ citas-react/src/components/Formulario.jsx
@@ -0,0 +1,13 @@
+//rfce (function declaration) - rafce (function expresion)
+
+const Formulario = () => {
+  return (
+    <div>
+      <h2>Formulario</h2>
+    </div>
+  )
+}
+
+export default Formulario
+
+
